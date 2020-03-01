b3003803b0089b88690362de46764a50d493ad55
Jonathan East <jonathan_east@ymail.com>
2020-03-01 04:32:08 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -2,3 +2,11 @@ function takeANumber(lineNumber, name) {
   lineNumber.push(name);
   return `Welcome, ${name}. You are number ${lineNumber.length} in line.`;
 }
+
+
+function nowServing(lineNumber) {
+  lineNumber.shift();
+  if(lineNumber.length === 0) {
+    return `There is nobody waiting to be served!`;
+  }
+}
