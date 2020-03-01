0e356d2e59ab8ae581bc45368505c6ad4b95f7e8
Jonathan East <jonathan_east@ymail.com>
2020-03-01 04:41:43 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -2,8 +2,3 @@ function takeANumber(lineNumber, name) {
   lineNumber.push(name);
   return `Welcome, ${name}. You are number ${lineNumber.length} in line.`;
 }
-
-
-function nowServing(lineNumber) {
-  return lineNumber.shift(name);
-}
