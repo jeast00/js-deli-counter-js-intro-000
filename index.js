44917a12951fc904b4975047d8a8b3d46d06a7eb
Jonathan East <jonathan_east@ymail.com>
2020-03-01 05:01:48 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -16,7 +16,5 @@ function nowServing(lineNumber) {
 function currentLine(line) {
   if(line.length === 0) {
     return `The line is currently empty.`;
-  } else {
-    return `The line is currently: ${line.length}, `;
   }
 }
