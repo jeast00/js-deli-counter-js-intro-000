ea57cd984731472b4035c2b970734447dbf1da1d
Jonathan East <jonathan_east@ymail.com>
2020-03-01 04:51:43 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ function nowServing(lineNumber) {
   if(lineNumber.length === 0) {
     return `There is nobody waiting to be served!`;
   } else {
-    return `Currently serving ${lineNumber}`;
+    return `Currently serving ${lineNumber.length}`;
   }
   lineNumber.shift();
 
