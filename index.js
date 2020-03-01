7b189339a9ca9cdcd6ea580d47d462c36324548f
Jonathan East <jonathan_east@ymail.com>
2020-03-01 04:58:02 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -17,6 +17,6 @@ function currentLine(line) {
   if(line.length === 0) {
     return `The line is currently empty.`;
   } else {
-    return `The line is currently: ${line.length}, `
+    return `The line is currently: ${line.length}, `;
   }
 }
